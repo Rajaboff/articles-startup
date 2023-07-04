3f71876ee6fd5a7077d528ce3dcd24725f2c04b2
Rajaboff <rajabov.manuchehr2001@gmail.com>
2023-07-04 18:23:38 +0500

    Создан и сверстан компонент card

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { CardComponent } from './card/card.component';
 
-const COMPONENTS = [HeaderComponent];
+const COMPONENTS = [HeaderComponent, CardComponent];
 
 @NgModule({
   declarations: [...COMPONENTS],
