16bb6390679f5768f430247fb0767c94be7dbdea
Rajaboff <rajabov.manuchehr2001@gmail.com>
2023-07-04 18:59:07 +0500

    Добавлены стили для кнопок и кнопка Создать в хедере

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { CardComponent } from './card/card.component';
+import { RouterModule } from '@angular/router';
 
 const COMPONENTS = [HeaderComponent, CardComponent];
 
 @NgModule({
   declarations: [...COMPONENTS],
   exports: [...COMPONENTS],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
 })
 export class ComponentsModule {}
